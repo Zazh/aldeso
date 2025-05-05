f1d979adee98b06f6073466df0d59f98e3662c8b
Zazh <pobiz@inbox.ru>
2025-05-06 04:23:26 +0500

    Add web frontend

--- web/postcss.config.mjs
+++ web/postcss.config.mjs
@@ -3,4 +3,11 @@ export default {
         "@tailwindcss/postcss": {},
         autoprefixer: {},
     },
+    theme: {
+        extend: {
+            gridTemplateColumns: {
+                '24': 'repeat(24, minmax(0, 1fr))',
+            }
+        }
+    }
 };
